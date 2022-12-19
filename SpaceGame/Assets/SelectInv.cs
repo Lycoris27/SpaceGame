a448e57e0a58b6a49e5630e7ae7624d13b018243
ET54321 <minecrafteret54321@gmail.com>
2022-12-19 14:08:21 +0800

    Collectables

--- SpaceGame/Assets/SelectInv.cs
+++ SpaceGame/Assets/SelectInv.cs
@@ -4,28 +4,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.HID;
 using UnityEngine.UI;
 
 public class SelectInv : MonoBehaviour
 {
-    public GameObject Inv;
+    private GameObject Inv;
     private RectTransform InvTrans;
 
-    public GameObject CraftInv;
+    private GameObject CraftInv;
+    private RectTransform CraftInvTrans;
 
-    public GameObject CompanionCrafter;
-    public GameObject Player;
+    private GameObject CompanionCrafter;
+    private GameObject Player;
+    private GameObject playerCamera;
     
-    public FirstPersonController PlayerControl;
+    private FirstPersonController PlayerControl;
 
-    public int DistanceCraft;
-    public int widthGrid;
-    public int heightGrid;
-    public int numHotbarSlot;
+    [SerializeField] private int DistanceCraft;
+    private int widthGrid;
+    private int heightGrid;
+    private int numHotbarSlot;
 
-    public GameObject ISprefab;
+    private int craftWidthGrid;
+    private int craftHeightGrid;
+
+    private int selectCollectableDist;
+
+    private GameObject ISprefab;
     private GameObject[,] slots;
     private GameObject[] HotbarSlot;
+    private GameObject[,] CraftingSlots;
+
+    private GameObject Collectables;
+
+    private GameObject plasticPrefab;
+    private GameObject[] plasticObj;
+    private int numPlastic;
+
+    //Crafting data such as number of a certain item
+    public int carryPlastic;
 
     void Start()
     {
@@ -43,10 +61,17 @@ public class SelectInv : MonoBehaviour
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
 
+        //Variables
         DistanceCraft = 4;
         heightGrid = 3;
         widthGrid = 5;
+        craftWidthGrid = 5;
+        craftHeightGrid = 4;
         numHotbarSlot = 3;
+        numPlastic = 10;
+        selectCollectableDist = 3;
+
+        carryPlastic = 0;
 
         //Generate all inventory slots (generated because its easier to keep track). 
         ISprefab = (GameObject)Resources.Load("InventorySlot");
@@ -62,7 +87,7 @@ public class SelectInv : MonoBehaviour
                 Vector3 temp = new Vector3(InvTrans.position.x - 350 + x * 100, 
                                            InvTrans.position.y + 300 - y * 100, //+ InvTrans.rect.height/2 - 300 - y * 100, 
                                            InvTrans.position.z);
-                slots[x, y] = (GameObject)Instantiate(ISprefab, temp, Quaternion.identity);
+                slots[x, y] = Instantiate(ISprefab, temp, Quaternion.identity);
                 slots[x, y].transform.parent = Inv.transform;
                 slots[x, y].name = "Slot_" + x + "_" + y;
 
@@ -78,18 +103,50 @@ public class SelectInv : MonoBehaviour
             Vector3 temp = new Vector3(Inv.transform.position.x - 250 + i * 100,
                                        InvTrans.position.y - 350,
                                        InvTrans.position.z);
-            HotbarSlot[i] = (GameObject)Instantiate(ISprefab, temp, Quaternion.identity);
+            HotbarSlot[i] = Instantiate(ISprefab, temp, Quaternion.identity);
             HotbarSlot[i].transform.parent = Inv.transform;
             HotbarSlot[i].name = "Hotbar_" + i;
 
             int tempI = i;
             HotbarSlot[i].GetComponent<Button>().onClick.AddListener(() => selectHotbar(tempI));
         }
+
+        //Crafting slots (select these to get a pop up of the materials needed to craft)
+        CraftingSlots = new GameObject[craftWidthGrid,craftHeightGrid];
+        CraftInvTrans = CraftInv.GetComponent<RectTransform>();
+        for (int x = 0; x < craftWidthGrid; x++)
+        {
+            for(int y = 0; y < craftHeightGrid; y++)
+            {
+                Vector3 temp = new Vector3(CraftInv.transform.position.x + x * 100,
+                                       CraftInv.transform.position.y - y * 100,
+                                       CraftInv.transform.position.z);
+                CraftingSlots[x, y] = Instantiate(ISprefab, temp, Quaternion.identity);
+                CraftingSlots[x, y].transform.parent = CraftInv.transform;
+                CraftingSlots[x, y].name = "CS_" + x + "_" + y;
+
+                int tempX = x, tempY = y;
+                CraftingSlots[x, y].GetComponent<Button>().onClick.AddListener(() => selectCraft(tempX, tempY));
+            }
+        }
+
+        Collectables = GameObject.Find("Collectables");
+
+        //Generate Plastic objects randomly
+        plasticPrefab = (GameObject)Resources.Load("PlasticObject");
+        plasticObj = new GameObject[numPlastic];
+        for(int plastic = 0; plastic < numPlastic; plastic++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-36, 16), 2, Random.Range(-34, 10));
+            plasticObj[plastic] = Instantiate(plasticPrefab, pos, Quaternion.identity);
+            plasticObj[plastic].transform.parent = Collectables.transform;
+            plasticObj[plastic].name = "Plastic_" + plastic;
+        }
     }
 
     void Update()
     {
-        if (Keyboard.current.eKey.wasPressedThisFrame)
+        if (Keyboard.current.tabKey.wasPressedThisFrame)
         {
             if (CompanionCrafter)
             {
@@ -115,6 +172,40 @@ public class SelectInv : MonoBehaviour
                 enterInvMenu();
             }
         }
+
+        if (Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            carryPlastic = checkGameObject(plasticObj, carryPlastic);
+        }
+    }
+
+    public int checkGameObject(GameObject[] Array, int numObject)
+    {
+        for (int i = 0; i < Array.Length; i++)
+        {
+            //Checking if its not removed
+            if (Array[i])
+            {
+                Vector3 tmp = Vector3.Normalize(Array[i].transform.position - Player.transform.position);
+                //Debug.Log(Vector3.Angle(tmp, Camera.main.transform.forward));
+
+                //Need to make sure facing is within certain angle as well
+                if (Vector3.Magnitude(Player.transform.position - Array[i].transform.position) < selectCollectableDist &&
+                    Vector3.Angle(tmp, Camera.main.transform.forward) < 15)
+                {
+                    numObject++;
+                    Destroy(Array[i]);
+                }
+            }
+        }
+
+        return numObject;
+    }
+
+    public void selectCraft(int x, int y)
+    {
+        GameObject craftingSlot = CraftingSlots[x, y];
+        Debug.Log(craftingSlot.name);
     }
 
     public void selectHotbar(int i)
